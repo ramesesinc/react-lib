9b3706404511e50f6f689a85b2699002f4f38fb6
meg <megp.rameses@gmail.com>
2025-08-19 09:41:11 +0800

    updated package

--- src/local-api/api.ts
+++ src/local-api/api.ts
@@ -1,77 +1,83 @@
-import { Platform } from "../common/platform";
 import AxiosBuilder, { AxiosBuilderCallback } from "../axios/axios-builder";
+import { Platform } from "../common/platform";
 
-const __LOCAL_API_KEY__ = "4e3b5a2c-9c74-4e10-9c76-3a3deec6d3c2"; 
+const __LOCAL_API_KEY__ = "4e3b5a2c-9c74-4e10-9c76-3a3deec6d3c2";
 
 const createAxiosClient = () => {
-    const contextPath = Platform.CONTEXT_PATH;
+  const contextPath = Platform.CONTEXT_PATH;
+  const callback: AxiosBuilderCallback = (config) => {
+    // Prepend the path if not already present
+    if (config.url) {
+      let urlPath = config.url;
 
-    const callback: AxiosBuilderCallback = ( config ) => {
-        // Prepend the path if not already present
-        if ( config.url ) {
-            let urlPath = config.url; 
-            if ( !urlPath.startsWith("/")) {
-                urlPath = `/${urlPath}`;
-            }
+      if (!urlPath.startsWith("/")) {
+        urlPath = `/${urlPath}`;
+      }
 
-            const prePaths = []; 
+      const prePaths = [];
 
-            if ( contextPath && !urlPath.startsWith( contextPath )) {
-                prePaths.push( contextPath ); 
-            }
+      if (contextPath && !urlPath.startsWith(contextPath)) {
+        prePaths.push(contextPath);
+      }
 
-            if ( !urlPath.startsWith("/localapi")) {
-                prePaths.push("localapi"); 
-            } 
+      if (!urlPath.startsWith("/localapi")) {
+        prePaths.push("localapi");
+      }
 
-            if ( prePaths && prePaths.length > 0 ) {
-                const prefix = prePaths.join("/");
-                prePaths.length = 0; 
+      if (prePaths && prePaths.length > 0) {
+        const prefix = prePaths.join("/");
+        prePaths.length = 0;
 
-                config.url = `${prefix}${urlPath}`;
-            }
+        config.url = `${prefix}${urlPath}`;
+        console.log("config.url ====> ", config.url);
+      }
 
-            if ( !config.url.startsWith("/")) {
-                config.url = `/${config.url}`
-            }
-            
-            console.log(`localAPI requestPath: ${config.url}`); 
-        }
+      if (!config.url.startsWith("/")) {
+        config.url = `/${config.url}`;
+      }
 
-        // Add the local API key header
-        if ( config.headers ) {
-            config.headers['X-LOCAL_API_KEY'] = __LOCAL_API_KEY__; 
-        }        
-    }; 
+      console.log(`localAPI requestPath: ${config.url}`);
+    }
 
-    return AxiosBuilder.build({ callback });
+    // Add the local API key header
+    if (config.headers) {
+      config.headers["X-LOCAL_API_KEY"] = __LOCAL_API_KEY__;
+      //   console.log("Headers being sent:", config.headers);
+    }
+  };
+
+  return AxiosBuilder.build({ callback });
 };
-
 
 const localAPI = {
+  client: createAxiosClient(),
 
-    client: createAxiosClient(), 
+  createAxiosClient,
 
-    createAxiosClient, 
-    
-    isPermitted: ( req: any ) => {
+  isPermitted: (req: any) => {
+    // console.log(req);
+    if (!req) return false;
 
-        if ( !req ) return false; 
+    let reqApiKey = null;
+    console.log("req.headers", req.headers["X-LOCAL_API_KEY"]);
+    if ("nextUrl" in req) {
+      // this is NextRequest object
+      reqApiKey = req.headers.get("X-LOCAL_API_KEY");
+      console.log("Passing next url", reqApiKey);
+    } else {
+      reqApiKey = req.get("X-LOCAL_API_KEY");
+      if (!reqApiKey) {
+        reqApiKey = req.headers["X-LOCAL_API_KEY"];
+      }
+      console.log("Passing default", reqApiKey);
+    }
 
-        let reqApiKey = null; 
-        if ( 'nextUrl' in req ) {
-            // this is NextRequest object 
-            reqApiKey = req.headers.get("X-LOCAL_API_KEY");
-        }
-        else {
-            reqApiKey = req.get("X-LOCAL_API_KEY");
-            if ( !reqApiKey ) {
-                reqApiKey = req.headers['X-LOCAL_API_KEY'];
-            }
-        }
+    return __LOCAL_API_KEY__ === reqApiKey;
+  },
 
-        return ( __LOCAL_API_KEY__ === reqApiKey ); 
-    },
+  addCustomHeaders: (headers: any) => {
+    headers.set("X-LOCAL_API_KEY", __LOCAL_API_KEY__);
+  },
 };
 
-export default localAPI; 
+export default localAPI;
