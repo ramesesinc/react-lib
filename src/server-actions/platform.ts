3b1f7710c0b243d03072ae60a7592aaaf602d4ad
meg <megp.rameses@gmail.com>
2025-10-22 14:37:29 +0800

    updated

--- src/server-actions/platform.ts
+++ src/server-actions/platform.ts
@@ -1,335 +1,363 @@
-'use server';
+"use server";
 
+import { getError } from "../axios/axios-utils";
 import { Platform } from "../index";
 import { mgmtAPI } from "../mgmt-api";
-import { getError } from "../axios/axios-utils";
+import { fetchSessionId, verifySessionToken } from "./session";
 
-// 
+//
 // connection actions
-// 
+//
 export async function getPlatform() {
-    try {
-        const client = mgmtAPI.createAxiosClient();
-        const resp = await client.get('/platform/info');
-        const { data } = resp ?? {};
-        return data;
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
-}
-
-export async function getConnection(connectionID: string, props?: { tenant?: string, module?: string }) {
-    try {
-        return await requestConnectionAction('GET', connectionID, props);
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
-}
-async function requestConnectionAction(type: string, connectionID: string, props?: { tenant?: string, module?: string }) {
-    let { tenant, module } = props ?? {};
-
-    const assertRequired = (name: string, value: any) => {
-        if ((value ?? '') === '') {
-            throw new Error(`${name} is required in actions/local#requestCollectionAction`);
-        }
-    }
-
-    tenant = (tenant ?? Platform.TENANT_NAME) ?? '';
-
-    assertRequired('tenant', tenant);
-    assertRequired('connectionID', connectionID);
-
+  try {
     const client = mgmtAPI.createAxiosClient();
-    const paths = ['/connections', tenant, module, connectionID];
-    const path = paths.filter((item) => (item ?? null !== null)).join("/");
-    const resp = await client.get(path);
+    const resp = await client.get("/platform/info");
     const { data } = resp ?? {};
     return data;
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
 }
 
+export async function getConnection(connectionID: string, props?: { tenant?: string; module?: string }) {
+  try {
+    return await requestConnectionAction("GET", connectionID, props);
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
+}
+async function requestConnectionAction(
+  type: string,
+  connectionID: string,
+  props?: { tenant?: string; module?: string }
+) {
+  let { tenant, module } = props ?? {};
 
-// 
+  const assertRequired = (name: string, value: any) => {
+    if ((value ?? "") === "") {
+      throw new Error(`${name} is required in actions/local#requestCollectionAction`);
+    }
+  };
+
+  tenant = tenant ?? Platform.TENANT_NAME ?? "";
+
+  assertRequired("tenant", tenant);
+  assertRequired("connectionID", connectionID);
+
+  const client = mgmtAPI.createAxiosClient();
+  const paths = ["/connections", tenant, module, connectionID];
+  const path = paths.filter((item) => item ?? null !== null).join("/");
+  const resp = await client.get(path);
+  const { data } = resp ?? {};
+  return data;
+}
+
+//
 // collection actions
-// 
-export async function getCollection(collection: string, action: string, props?: { tenant?: string, module?: string }) {
-    try {
-        return await requestCollectionAction('GET', collection, action, {}, props);
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
+//
+export async function getCollection(collection: string, action: string, props?: { tenant?: string; module?: string }) {
+  try {
+    return await requestCollectionAction("GET", collection, action, {}, props);
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
 }
 
-export async function postCollection(collection: string, action: string, body: Record<string, any>, props?: { tenant?: string, module?: string }) {
-    try {
-        return await requestCollectionAction('POST', collection, action, body, props);
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
+export async function postCollection(
+  collection: string,
+  action: string,
+  body: Record<string, any>,
+  props?: { tenant?: string; module?: string }
+) {
+  try {
+    return await requestCollectionAction("POST", collection, action, body, props);
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
 }
 
-async function requestCollectionAction(type: string, collection: string, action: string, body: Record<string, any>, props?: { tenant?: string, module?: string }) {
-    let { tenant, module } = props ?? {};
+async function requestCollectionAction(
+  type: string,
+  collection: string,
+  action: string,
+  body: Record<string, any>,
+  props?: { tenant?: string; module?: string }
+) {
+  let { tenant, module } = props ?? {};
 
-    const assertRequired = (name: string, value: any) => {
-        if ((value ?? '') === '') {
-            throw new Error(`${name} is required in actions/local#requestCollectionAction`);
-        }
+  const assertRequired = (name: string, value: any) => {
+    if ((value ?? "") === "") {
+      throw new Error(`${name} is required in actions/local#requestCollectionAction`);
     }
+  };
 
-    tenant = (tenant ?? Platform.TENANT_NAME) ?? '';
+  tenant = tenant ?? Platform.TENANT_NAME ?? "";
 
-    assertRequired('tenant', tenant);
-    assertRequired('collection', collection);
-    assertRequired('action', action);
+  assertRequired("tenant", tenant);
+  assertRequired("collection", collection);
+  assertRequired("action", action);
 
-    const resolveResult = (resp: any) => {
-        const { data } = resp ?? {};
-        return data;
-    }
+  const resolveResult = (resp: any) => {
+    const { data } = resp ?? {};
+    return data;
+  };
 
-    const client = mgmtAPI.createAxiosClient();
-    const paths = ['/mgmt', tenant, module, collection, action];
-    const path = paths.filter((item) => (item != null)).join("/");
-    if (type === 'GET') {
-        return resolveResult(await client.get(path));
-    } else {
-        return resolveResult(await client.post(path, body));
-    }
+  const client = mgmtAPI.createAxiosClient();
+  const paths = ["/mgmt", tenant, module, collection, action];
+  const path = paths.filter((item) => item != null).join("/");
+  if (type === "GET") {
+    return resolveResult(await client.get(path));
+  } else {
+    return resolveResult(await client.post(path, body));
+  }
 }
 
-
-// 
+//
 // modules actions
-// 
+//
 export async function getModule(moduleID: string, props?: { tenant?: string }) {
-    try {
-        if (moduleID === 'list') {
-            throw new Error(`'${moduleID}' is not a valid moduleID`);
-        }
-        return await fetchModuleData(moduleID, props);
+  try {
+    if (moduleID === "list") {
+      throw new Error(`'${moduleID}' is not a valid moduleID`);
     }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
+    return await fetchModuleData(moduleID, props);
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
 }
 
 export async function getModules(props?: { tenant?: string }) {
-    try {
-        return await fetchModuleData('list', props);
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
+  try {
+    return await fetchModuleData("list", props);
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
 }
 
 async function fetchModuleData(moduleID: string, props?: { tenant?: string }) {
-    let { tenant } = props ?? {};
+  let { tenant } = props ?? {};
 
-    const assertRequired = (name: string, value: any) => {
-        if ((value ?? '') === '') {
-            throw new Error(`${name} is required in actions/local#fetchModuleData`);
-        }
+  const assertRequired = (name: string, value: any) => {
+    if ((value ?? "") === "") {
+      throw new Error(`${name} is required in actions/local#fetchModuleData`);
     }
+  };
 
-    tenant = (tenant ?? Platform.TENANT_NAME) ?? '';
+  tenant = tenant ?? Platform.TENANT_NAME ?? "";
 
-    assertRequired('tenant', tenant);
-    assertRequired('moduleID', moduleID);
+  assertRequired("tenant", tenant);
+  assertRequired("moduleID", moduleID);
 
-    const client = mgmtAPI.createAxiosClient();
-    const path = `/modules/${tenant}/${moduleID}`;
-    const resp = await client.get(path);
+  const client = mgmtAPI.createAxiosClient();
+  const path = `/modules/${tenant}/${moduleID}`;
+  const resp = await client.get(path);
+  const { data } = resp ?? {};
+  return data;
+}
+
+//
+// service actions
+//
+export async function resolveService(
+  serviceID: string,
+  action: string,
+  props: { module: string; tenant?: string; platform?: string }
+) {
+  try {
+    return await requestServiceAction("resolve", serviceID, action, {}, props);
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
+}
+export async function invokeService(
+  serviceID: string,
+  action: string,
+  body: Record<string, any>,
+  props: { module: string; tenant?: string; platform?: string }
+) {
+  try {
+    const token = await fetchSessionId();
+    if (token != null && token.trim() !== "") {
+      const tokenData = await verifySessionToken(token);
+      const newBody = {
+        env: {
+          ...(tokenData ?? {}),
+          CLIENTTYPE: "web",
+          USER_AGENT: `react-web/${process.env.PLATFORM_NAME}`,
+        },
+        args: body,
+      };
+      body = newBody;
+      console.log("BODY", body);
+    }
+    return await requestServiceAction("invoke", serviceID, action, body, props);
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
+}
+
+async function requestServiceAction(
+  type: string,
+  serviceID: string,
+  action: string,
+  body: Record<string, any>,
+  props: { module: string; tenant?: string; platform?: string }
+) {
+  let { tenant, module } = props ?? {};
+
+  const assertRequired = (name: string, value: any) => {
+    if ((value ?? "") === "") {
+      throw new Error(`${name} is required in actions/local#requestServiceAction`);
+    }
+  };
+
+  tenant = tenant ?? Platform.TENANT_NAME ?? "";
+
+  assertRequired("tenant", tenant);
+  assertRequired("module", module);
+  assertRequired("serviceID", serviceID);
+  assertRequired("action", action);
+
+  const client = mgmtAPI.createAxiosClient();
+  const path = `/apis/${type}/${tenant}/${module}/${serviceID}/${action}`;
+  console.log({ type, path });
+  const resolveResult = (resp: any) => {
     const { data } = resp ?? {};
     return data;
+  };
+
+  if (type === "resolve") {
+    return resolveResult(await client.get(path));
+  } else {
+    return resolveResult(await client.post(path, body));
+  }
 }
 
-
-// 
-// service actions
-// 
-export async function resolveService(serviceID: string, action: string, props: { module: string, tenant?: string, platform?: string }) {
-    try {
-        return await requestServiceAction('resolve', serviceID, action, {}, props);
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
-}
-export async function invokeService(serviceID: string, action: string, body: Record<string, any>, props: { module: string, tenant?: string, platform?: string }) {
-    try {
-        return await requestServiceAction('invoke', serviceID, action, body, props);
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
-}
-
-async function requestServiceAction(type: string, serviceID: string, action: string, body: Record<string, any>, props: { module: string, tenant?: string, platform?: string }) {
-    let { tenant, module } = props ?? {};
-
-    const assertRequired = (name: string, value: any) => {
-        if ((value ?? '') === '') {
-            throw new Error(`${name} is required in actions/local#requestServiceAction`);
-        }
-    }
-
-    tenant = (tenant ?? Platform.TENANT_NAME) ?? '';
-
-    assertRequired('tenant', tenant);
-    assertRequired('module', module);
-    assertRequired('serviceID', serviceID);
-    assertRequired('action', action);
-
-    const client = mgmtAPI.createAxiosClient();
-    const path = `/apis/${type}/${tenant}/${module}/${serviceID}/${action}`;
-    console.log({ type, path }); 
-    const resolveResult = (resp: any) => {
-        const { data } = resp ?? {};
-        return data;
-    }
-
-    if (type === 'resolve') {
-        return resolveResult(await client.get(path));
-    } else {
-        return resolveResult(await client.post(path, body));
-    }
-}
-
-
-// 
+//
 // tenant actions
-// 
+//
 export async function getTenant(tenant?: string) {
-    try {
-        tenant = (tenant ?? Platform.TENANT_NAME) ?? '';
-        if (tenant === 'list') {
-            throw new Error(`'${tenant}' is not a valid tenant`);
-        }
-        return await requestTenantAction(tenant);
+  try {
+    tenant = tenant ?? Platform.TENANT_NAME ?? "";
+    if (tenant === "list") {
+      throw new Error(`'${tenant}' is not a valid tenant`);
     }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
+    return await requestTenantAction(tenant);
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
 }
 export async function getTenants() {
-    try {
-        return await requestTenantAction('list');
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
+  try {
+    return await requestTenantAction("list");
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
 }
 async function requestTenantAction(tenant: string) {
-
-    const assertRequired = (name: string, value: any) => {
-        if ((value ?? '') === '') {
-            throw new Error(`${name} is required in actions/local#requestTenantAction`);
-        }
+  const assertRequired = (name: string, value: any) => {
+    if ((value ?? "") === "") {
+      throw new Error(`${name} is required in actions/local#requestTenantAction`);
     }
+  };
 
-    assertRequired('tenant', tenant);
+  assertRequired("tenant", tenant);
 
-    const client = mgmtAPI.createAxiosClient();
-    const path = `/tenants/${tenant}`;
-    const resp = await client.get(path);
+  const client = mgmtAPI.createAxiosClient();
+  const path = `/tenants/${tenant}`;
+  const resp = await client.get(path);
+  const { data } = resp ?? {};
+  return data;
+}
+
+//
+// datasource actions
+//
+export async function getDataSource(dataSourceID: string, props?: { tenant?: string; module?: string }) {
+  try {
+    if (dataSourceID === "list") {
+      throw new Error(`'${dataSourceID}' is not a valid DataSource`);
+    }
+    return await requestDataSourceAction("GET", dataSourceID, props);
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
+}
+export async function getDataSources(props?: { tenant?: string; module?: string }) {
+  try {
+    return await requestDataSourceAction("GET", "list", props);
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
+}
+async function requestDataSourceAction(
+  type: string,
+  dataSourceID: string,
+  props?: { tenant?: string; module?: string }
+) {
+  let { tenant, module } = props ?? {};
+
+  const assertRequired = (name: string, value: any) => {
+    if ((value ?? "") === "") {
+      throw new Error(`${name} is required in actions/local#requestDataSourceAction`);
+    }
+  };
+
+  tenant = tenant ?? Platform.TENANT_NAME ?? "";
+
+  assertRequired("tenant", tenant);
+  assertRequired("dataSourceID", dataSourceID);
+
+  const client = mgmtAPI.createAxiosClient();
+  const paths = ["/datasources", tenant, module, dataSourceID];
+  const path = paths.filter((item) => item ?? null !== null).join("/");
+
+  const resolveResult = (resp: any) => {
     const { data } = resp ?? {};
     return data;
+  };
+
+  if (type === "GET") {
+    return resolveResult(await client.get(path));
+  } else {
+    throw new Error(`'${type}' not supported in actions/local#requestDataSourceAction`);
+  }
 }
-
-
-// 
-// datasource actions
-// 
-export async function getDataSource(dataSourceID: string, props?: { tenant?: string, module?: string }) {
-    try {
-        if (dataSourceID === 'list') {
-            throw new Error(`'${dataSourceID}' is not a valid DataSource`);
-        }
-        return await requestDataSourceAction('GET', dataSourceID, props);
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
-}
-export async function getDataSources(props?: { tenant?: string, module?: string }) {
-    try {
-        return await requestDataSourceAction('GET', 'list', props);
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
-}
-async function requestDataSourceAction(type: string, dataSourceID: string, props?: { tenant?: string, module?: string }) {
-    let { tenant, module } = props ?? {};
-
-    const assertRequired = (name: string, value: any) => {
-        if ((value ?? '') === '') {
-            throw new Error(`${name} is required in actions/local#requestDataSourceAction`);
-        }
-    }
-
-    tenant = (tenant ?? Platform.TENANT_NAME) ?? '';
-
-    assertRequired('tenant', tenant);
-    assertRequired('dataSourceID', dataSourceID);
-
-    const client = mgmtAPI.createAxiosClient();
-    const paths = ['/datasources', tenant, module, dataSourceID];
-    const path = paths.filter((item) => (item ?? null !== null)).join("/");
-
-    const resolveResult = (resp: any) => {
-        const { data } = resp ?? {};
-        return data;
-    }
-
-    if (type === 'GET') {
-        return resolveResult(await client.get(path));
-    } else {
-        throw new Error(`'${type}' not supported in actions/local#requestDataSourceAction`);
-    }
-}
-
 
 //
 // generic actions to the platform-server
 //
 export async function platformPostData(path: string, body: Record<string, any>) {
-    try {
-        const client = mgmtAPI.createAxiosClient();
-        const resp = await client.post( path, body ); 
-        const { data } = resp ?? {}; 
-        return data;  
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
+  try {
+    const client = mgmtAPI.createAxiosClient();
+    const resp = await client.post(path, body);
+    const { data } = resp ?? {};
+    return data;
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
 }
 export async function platformGetData(path: string) {
-    try {
-        const client = mgmtAPI.createAxiosClient();
-        const resp = await client.get( path );
-        const { data } = resp ?? {}; 
-        return data;  
-    }
-    catch (err) {
-        const e = getError(err);
-        throw new Error(e.message);
-    }
+  try {
+    const client = mgmtAPI.createAxiosClient();
+    const resp = await client.get(path);
+    const { data } = resp ?? {};
+    return data;
+  } catch (err) {
+    const e = getError(err);
+    throw new Error(e.message);
+  }
 }
