216a59963939dc155dfcbb6cd02c0adcbbe89f3d
worgflores <worgflores@gmail.com>
2025-10-29 13:03:30 +0800

    added functionality to build session headers

--- src/server-actions/session.ts
+++ src/server-actions/session.ts
@@ -2,6 +2,7 @@
 
 import jwt from 'jsonwebtoken';
 import { cookies } from 'next/headers';
+import { AES } from '../common/utils';
 
 const SESSION_KEY = "MDQyNjZiZGY2NmRjNjcwNGMzYTQ5YjJkMDM1YTA3MGQvM2Y3OTJmMjRiZGQyOTlmMWMxNjNmMjM1OGZiMTMwZjk="; 
 
@@ -48,4 +49,24 @@ export async function clearSessionId(): Promise<void> {
     maxAge: 0, // expires immediately
     secure: process.env.NODE_ENV === 'production',
   });
+}
+
+export async function buildSessionHeader(): Promise<Record<string, any>> {
+  const key = 'x-session-token';
+  const headers = { [key]: '' };
+  try {
+    const token = await fetchSessionId();
+    if (token == null || token.trim() === "") {
+      return headers;
+    }
+
+    const data = await verifySessionToken(token);
+    if (data == null) return headers;
+
+    headers[key] = AES.encrypt(data);
+    return headers;
+  }
+  catch (err) {
+    return headers;
+  }
 }
